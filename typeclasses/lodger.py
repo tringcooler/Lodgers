f1864428c2e5684ae4b2b87c48fe69b1e1fcee77
tringcooler <tringcooler@users.noreply.github.com>
2016-11-09 05:28:16 +0800

    Update lodger.py

--- typeclasses/lodger.py
+++ typeclasses/lodger.py
@@ -14,53 +14,80 @@ class Lodger(Character):
         self.db.ability = {}
         self.db.status = {
             'attention_pool': (0, 0, None),
-            'attention_locked': ([], []),
+            'attention_locked': {},
             }
 
     def _set_attention_pool(self, val, target = None, time = None):
         if not time:
             time = get_time()
+        if not target:
+            _t, _t, target = self.db.status['attention_pool']
         self.db.status['attention_pool'] = (val, time, target)
 
     def _get_attention_pool(self, time = None):
+        val, stamp, target = self.db.status['attention_pool']
         if not time:
             time = get_time()
-        val, stamp, _t = self.db.status['attention_pool']
         timedif = max(int(time - stamp), 0)
         new_val = min(val - timedif * self.db.property['attention_regen'], 0)
-        return new_val
+        return new_val, target
 
     def _set_attention_locked(self, val, target):
         attention_locked = self.db.status['attention_locked']
-        if target in attention_locked[0]:
-            idx = attention_locked[0].index(target)
-        else:
-            idx = len(attention_locked[0])
-            attention_locked[0].append(target)
-            attention_locked[1].append(0)
-        attention_locked[1][idx] += val
-        if attention_locked[1][idx] <= 0:
-            del attention_locked[0][idx]
-            del attention_locked[1][idx]
+        if not target in attention_locked:
+            attention_locked[target] = 0
+        attention_locked[target] += val
+        if attention_locked[target] <= 0:
+            del attention_locked[target]
 
-    def _set_attention_locked(self, val, target):
+    def _get_attention_locked_all(self):
+        attention = 0
+        for target in attention_locked:
+            attention += attention_locked[target]
+        return attention
+
+    def _get_attention_locked(self, target):
         attention_locked = self.db.status['attention_locked']
-        if not target in attention_locked[0]:
+        if not target in attention_locked:
             return 0
-        idx = attention_locked[0].index(target)
-        return attention_locked[1][idx]
+        return attention_locked[target]
 
-    def pay_attention(self, val, target, force = False):
-        _t, _t, o_target = self.db.status['attention']
+    def _calc_attention_cost(self, val, target, time = None):
+        # |              pool              |
+        # |      used      |               |
+        # |       target == o_target :     |
+        # |  val  | remain |               |
+        # |        val          |          |
+        # |       target != o_target :     |
+        # |     remain     |   val    |    |
+        attention = self.db.property['attention_max']
+        attention -= self._get_attention_locked_all()
+        attention += self._get_attention_locked(target)
+        attention_used, o_target = self._get_attention_pool(time = time)
         if target == o_target:
-            attention = self.db.property['attention_max']
+            cost = max(val, attention_used)
+            remain = min(attention_used - val, 0)
         else:
-            attention = self.get_attention()
-        if attention < val and not force:
+            cost = val + attention_used
+            remain = attention_used
+        return attention, cost, remain
+
+    def pay_attention(self, val, target, force = False, time = None):
+        attention, cost, _t = self._calc_attention_cost(val, target, time = time)
+        if attention < cost and not force:
             return False
-        attention = max(attention - val, 0)
-        self.set_attention(attention, target)
+        cost = min(cost, attention)
+        self._set_attention_pool(cost, target, time = time)
         return True
+
+    def lock_attention(self, val, target, force = False, time = None):
+        attention, cost, remain = self._calc_attention_cost(val, target, time = time)
+        if attention < cost and not force:
+            return False
+        pass
+
+    def free_attention(self, val, target, force = False, time = None):
+        pass
         
 class LodgerAction(object):
 
@@ -73,3 +100,6 @@ class LodgerAction(object):
         if not caller.pay_attention(
             self.attention, target, self.positive):
             return False
+        
+        
+
