eceaa2e36309811cb7ff3f45a6830e9ba4a67c83
tringcooler <tringcooler@users.noreply.github.com>
2016-11-30 11:53:07 +0800

    Update lodger.py

--- typeclasses/lodger.py
+++ typeclasses/lodger.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from time import time as get_time
-from evennia import create_object
+from evennia import create_object, search_object
 from typeclasses.characters import Character
+from typeclasses.objects import Object as BaseObject
+from commands.command import Command
 
 class Lodger(Character):
 
@@ -11,10 +13,19 @@ class Lodger(Character):
             'attention_max': 0,
             'attention_regen': 0,
             }
-        self.db.ability = {}
+        self.db.ability = {
+            'auto_reaction': {},
+            }
         self.db.status = {
             'attention_pool': (0, 0, None),
             'attention_locked': {},
+            'action_handle': {
+                'eye': None,
+                'ear': None,
+                'mouth': None,
+                'hand': None,
+                'foot': None,
+                },
             }
 
     def _set_attention_pool(self, val, target = None, time = None):
@@ -124,18 +135,122 @@ class Lodger(Character):
         val += attention_pool
         val = min(val, attention)
         self._set_attention_pool(val, target, time = time)
+
+    def on_trigger(self, info):
+        pass
+
+    def feel(source, content):
+        self.msg('{0}:{1}'.format(source, content))
         
 class LodgerAction(object):
 
-    def __init__(self):
-        self.attention = 0
-        self.positive = True
-        self.triger = None
+    desc = None
+    trigger_room_only = False
+    trigger_volume = 0
+    trigger_volume_reduce = 10
 
-    def execute(self, caller, target):
-        if not caller.pay_attention(
-            self.attention, target, self.positive):
-            return False
+    def __init__(self):
+        pass
+    
+    def description(self):
+        if self.desc:
+            return self.desc
+        else:
+            return self
+
+    def on_trigger(self, info):
+        info['volum'] = max(0,
+            self.trigger_volume - self.trigger_volume_reduce * info['distance'])
+        return info
+
+    def on_reaction(self, action, caller, info):
+        pass
+
+    def trigger(self, caller):
+        s_room = caller.location
+        lodge = s_room.db.instance
+        for room in lodge.db.rooms:
+            info = {
+                'caller': caller,
+                'action': self.description(),
+                }
+            if room == s_room:
+                info['distance'] = 0
+            else:
+                if self.trigger_room_only:
+                    continue
+                info['distance'] = room.calc_distance(s_room)
+            info = self.on_triggrt(info)
+            if room != s_room:
+                info['source'] = s_room
+                info['destination'] = room
+                info = s_room.on_trigger(info)
+                info = room.on_trigger(info)
+            for obj in room.contents:
+                if hasattr(obj, 'on_trigger'):
+                    obj.on_trigger(info)
+
+    def reaction(self, action, caller, info):
+        return self.on_reaction(action, caller, info)
+
+    def execute(self, caller, info):
+        pass
+
+TXT_ACTION_CN = {
+    'look': {
+        'error': {
+            'nothing': '不知道应该看向哪里',
+            }
+        }
+    }
+TXT_ACTION = TXT_ACTION_CN
+
+class LookAction(LodgerAction):
+    
+    desc = 'look'
+    trigger_room_only = True
+
+    def __init__(self):
+        super(LodgerAction, self).__init__()
+
+    def on_reaction(self, action, caller, info):
+        if action == 'look':
+            pass
+
+    def execute(self, caller, info):
+        if not 'target' in info:
+            caller.feel('error', TXT_ACTION['look']['error']['nothing'])
+            return
+        target = info['target']
+        pass
+        caller.feel('look', content)
+
+g_action_list_key = None
+def create_action_list(key):
+    if g_action_list_key:
+        raise RuntimeError('action list already exist')
+    al = create_object(LodgeActionList, key = key)
+    g_action_list_key = key
+    return al
+
+def get_action_list():
+    if not g_action_list_key:
+        raise RuntimeError('action list not exist')
+    return search_object(g_action_list_key)[0]
+
+def get_action(desc):
+    return get_action_list().get_action(desc)
         
-        
+class LodgeActionList(BaseObject):
+
+    def at_object_creation(self):
+        self.db.actions = {}
+
+    def create_action(self, action):
+        if action.desc in self.db.actions:
+            raise RuntimeError('action already exist')
+        self.db.actions[action.desc] = action()
+
+    def get_action(self, desc):
+        return self.db.actions[desc]
 
